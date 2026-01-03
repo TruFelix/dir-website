acf366e36481a3621472d386174f93294c8eb163
truFelix <trutschnigfelix@gmail.com>
2026-01-03 12:55:20 +0100

    nojekyll & assetPath & unoptimized images
    Signed-off-by: truFelix <trutschnigfelix@gmail.com>

--- evar/next.config.ts
+++ evar/next.config.ts
@@ -5,7 +5,11 @@ const nextConfig: NextConfig = {
   output: "export",
   distDir: "../docs",
   transpilePackages: ["common"],
-  basePath: '/dir-website'
+  basePath: '/dir-website',
+  assetPrefix: '/dir-website/',
+  images: {
+    unoptimized: true, // GitHub Pages hat keine Image-Optimization-API
+  },
 };
 
 export default nextConfig;
